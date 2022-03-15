fa2b4ac01c589d6ec76dd345feba7a763034405e
Joe Harrison <github@joeharrison.com>
2022-03-15 13:26:47 -0400

    Update cats.py

--- cats/cats.py
+++ cats/cats.py
@@ -185,7 +185,7 @@ CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 )
 @click.option(
     "-p",
-    "--pushtx",
+    "--push",
     is_flag=True,
     help="Automatically push transaction to the network in quiet mode",
 )
@@ -203,7 +203,7 @@ def cli(
     as_bytes: bool,
     select_coin: bool,
     quiet: bool,
-    pushtx: bool
+    push: bool
 ):
     ctx.ensure_object(dict)
 
@@ -285,7 +285,7 @@ def cli(
     else:
         final_bundle_dump = json.dumps(final_bundle.to_json_dict(), sort_keys=True, indent=4)
 
-    confirmation = pushtx
+    confirmation = push
 
     if not quiet:
         confirmation = input(
