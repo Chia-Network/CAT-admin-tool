4d1f8f451f63cb9170c9022f69f7ab3f5e115288
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-07-02 14:04:53 +0100

    initial batch the bag implementation

--- cats/secure_the_bag.py
+++ cats/secure_the_bag.py
@@ -2,8 +2,9 @@ from typing import List, Tuple
 
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.types.condition_opcodes import ConditionOpcode
+from chia.util.hash import std_hash
 from chia.util.ints import uint64
-
+from clvm.casts import int_to_bytes
 
 class Target:
     puzzle_hash: bytes32
@@ -23,5 +24,39 @@ def batch_the_bag(targets: List[Target], leaf_width: int) -> List[Target]:
     puzzle hash and amount in a batch
     """
     results = []
+    batch_cache = {}
+    current_batch = []
+    collisions = []
+
+    for index, target in enumerate(targets):
+        remaining_collisions = []
+        for collision in collisions:
+            key = std_hash(collision.puzzle_hash + int_to_bytes(collision.amount))
+
+            if batch_cache.get(key):
+                remaining_collisions.append(collision)
+            else:
+                current_batch.append(collision)
+                batch_cache[key] = True
+            
+            if len(current_batch) == leaf_width:
+                results.append(current_batch)
+                batch_cache = {}
+                current_batch = []
+        
+        collisions = remaining_collisions
+
+        key = std_hash(target.puzzle_hash + int_to_bytes(target.amount))
+
+        if batch_cache.get(key):
+            collisions.append(target)
+        else:
+            current_batch.append(target)
+            batch_cache[key] = True
+        
+        if len(current_batch) == leaf_width or index == len(targets) - 1:
+            results.append(current_batch)
+            batch_cache = {}
+            current_batch = []
 
     return results
