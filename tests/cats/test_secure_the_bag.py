59fee36b5dbf0af6e3a3bc4feb070a081835fff6
Freddie Coleman <f.coleman@hotmail.co.uk>
2022-07-13 10:20:49 +0100

    update tests for bugfix where secured bag of CATs was the inner puzzle to a CAT

--- tests/cats/test_secure_the_bag.py
+++ tests/cats/test_secure_the_bag.py
@@ -244,90 +244,78 @@ def test_secure_bag_of_cats():
     root_hash, parent_puzzle_lookup = secure_the_bag(targets, 2, asset_id)
 
     # Calculates correct root hash
-    assert root_hash.hex() == "6c796264932252139b2aba1dc2e17557256a969b843c64af4b91c8eba2ffc5a4"
+    assert root_hash.hex() == "bbffed16fdfe5b4c79fced8d04d913a68ea4a028e843e4fb09df18d432713810"
 
     node_1_inner_puzzle_hash = bytes32.fromhex("59e9003d6199915750187188a631187d0bb78b32a1ae04155615cae267975b4b")
     node_1_outer_puzzle_hash = construct_cat_puzzle(CAT_MOD, asset_id, node_1_inner_puzzle_hash).get_tree_hash(node_1_inner_puzzle_hash)
     node_2_inner_puzzle_hash = bytes32.fromhex("db49cd4d6507dada1f3407ebda46586d5254f8f29bbe6d360ef1b87a4c3100f8")
     node_2_outer_puzzle_hash = construct_cat_puzzle(CAT_MOD, asset_id, node_2_inner_puzzle_hash).get_tree_hash(node_2_inner_puzzle_hash)
 
-    root_puzzle = construct_cat_puzzle(
-        CAT_MOD,
-        asset_id,
-        Program.to(
-            (
-                1,
+    root_puzzle = Program.to(
+        (
+            1,
+            [
+                [ConditionOpcode.CREATE_COIN_ANNOUNCEMENT, None],
                 [
-                    [ConditionOpcode.CREATE_COIN_ANNOUNCEMENT, None],
-                    [
-                        ConditionOpcode.CREATE_COIN,
-                        node_1_inner_puzzle_hash,
-                        uint64(10000032100000000),
-                        [node_1_inner_puzzle_hash]
-                    ],
-                    [
-                        ConditionOpcode.CREATE_COIN,
-                        node_2_inner_puzzle_hash,
-                        uint64(10000000000000000),
-                        [node_2_inner_puzzle_hash]
-                    ]
+                    ConditionOpcode.CREATE_COIN,
+                    node_1_inner_puzzle_hash,
+                    uint64(10000032100000000),
+                    [node_1_inner_puzzle_hash]
+                ],
+                [
+                    ConditionOpcode.CREATE_COIN,
+                    node_2_inner_puzzle_hash,
+                    uint64(10000000000000000),
+                    [node_2_inner_puzzle_hash]
                 ]
-            )
+            ]
         )
     )
 
     # Puzzle reveal for root hash is correct
     assert root_puzzle.get_tree_hash().hex() == root_hash.hex()
 
-    node_1_puzzle = construct_cat_puzzle(
-        CAT_MOD,
-        asset_id,
-        Program.to(
-            (
-                1,
+    node_1_puzzle = Program.to(
+        (
+            1,
+            [
+                [ConditionOpcode.CREATE_COIN_ANNOUNCEMENT, None],
                 [
-                    [ConditionOpcode.CREATE_COIN_ANNOUNCEMENT, None],
-                    [
-                        ConditionOpcode.CREATE_COIN,
-                        target_1_inner_puzzle_hash,
-                        target_1_amount,
-                        [target_1_inner_puzzle_hash]
-                    ],
-                    [
-                        ConditionOpcode.CREATE_COIN,
-                        target_2_inner_puzzle_hash,
-                        target_2_amount,
-                        [target_2_inner_puzzle_hash]
-                    ]
+                    ConditionOpcode.CREATE_COIN,
+                    target_1_inner_puzzle_hash,
+                    target_1_amount,
+                    [target_1_inner_puzzle_hash]
+                ],
+                [
+                    ConditionOpcode.CREATE_COIN,
+                    target_2_inner_puzzle_hash,
+                    target_2_amount,
+                    [target_2_inner_puzzle_hash]
                 ]
-            )
+            ]
         )
     )
 
     # Puzzle reveal for node 1 is correct
-    assert node_1_puzzle.get_tree_hash().hex() == node_1_outer_puzzle_hash.hex()
+    assert node_1_puzzle.get_tree_hash().hex() == node_1_inner_puzzle_hash.hex()
 
-    node_2_puzzle = construct_cat_puzzle(
-        CAT_MOD,
-        asset_id,
-        Program.to(
-            (
-                1,
+    node_2_puzzle = Program.to(
+        (
+            1,
+            [
+                [ConditionOpcode.CREATE_COIN_ANNOUNCEMENT, None],
                 [
-                    [ConditionOpcode.CREATE_COIN_ANNOUNCEMENT, None],
-                    [
-                        ConditionOpcode.CREATE_COIN,
-                        target_3_inner_puzzle_hash,
-                        target_3_amount,
-                        [target_3_inner_puzzle_hash]
-                    ]
+                    ConditionOpcode.CREATE_COIN,
+                    target_3_inner_puzzle_hash,
+                    target_3_amount,
+                    [target_3_inner_puzzle_hash]
                 ]
-            )
+            ]
         )
     )
 
     # Puzzle reveal for node 2 is correct
-    assert node_2_puzzle.get_tree_hash().hex() == node_2_outer_puzzle_hash.hex()
+    assert node_2_puzzle.get_tree_hash().hex() == node_2_inner_puzzle_hash.hex()
 
     # Parent puzzle lookup (used for puzzle reveals)
     puzzle_create_target_1 = parent_puzzle_lookup.get(target_1_outer_puzzle_hash.hex())
@@ -345,8 +333,8 @@ def test_secure_bag_of_cats():
     puzzle_create_node_2 = parent_puzzle_lookup.get(node_2_outer_puzzle_hash.hex())
 
     # Nodes 1 & 2 are created by spending root
-    assert puzzle_create_node_1.puzzle.get_tree_hash().hex() == root_hash.hex()
-    assert puzzle_create_node_2.puzzle.get_tree_hash().hex() == root_hash.hex()
+    assert puzzle_create_node_1.puzzle.get_tree_hash().hex() == construct_cat_puzzle(CAT_MOD, asset_id, root_puzzle).get_tree_hash().hex()
+    assert puzzle_create_node_2.puzzle.get_tree_hash().hex() == construct_cat_puzzle(CAT_MOD, asset_id, root_puzzle).get_tree_hash().hex()
 
 def test_parent_of_puzzle_hash():
     target_1_puzzle_hash = bytes32.fromhex("4bc6435b409bcbabe53870dae0f03755f6aabb4594c5915ec983acf12a5d1fba")
